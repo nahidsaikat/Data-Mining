597e0c0bb6e37acc44b25769b0516ad30b8bd0b5
MD. Nahidur Rahman <nahidsaikatft40@gmail.com>
2018-03-27 03:59:39 +0600

    Create test.py
    test file

---
+++ test.py
@@ -0,0 +1,12 @@
+from exercise import DBScanner
+
+scanner = DBScanner(0.1, 5)
+scanner.db_scan()
+
+test_case_1 = set([1, 2, 3, 4, 5, 6, 7, 8, 9, -1])
+clusters = scanner.get_clusters()
+
+print 'Test Case # 1'
+print test_case_1
+print clusters
+
